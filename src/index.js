04ffd1d5cd8b2e12c0576aba0585fbcdb797805e
Denys iElkin <hvoarang@gmail.com>
2023-11-03 04:18:26 +0200

    asd

--- src/index.js
+++ src/index.js
@@ -17,9 +17,9 @@ console.log(element);
 render(element, document.querySelector('#root'));
 
 const arr = [1, 2, 3, 4, 5, 6, 7];
-
-arr.splice(1.3);
-const newArr = arr.slice(2, 2);
+const newArr = arr.slice(0, 2);
 
 console.log(newArr);
 console.log(`arrey is  ${arr}`);
+
+let ary = [1, 2, 3];
